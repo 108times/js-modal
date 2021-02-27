71d807388acb7b43752be680ef17ec838de0671d
Amir <amirkadenov@gmail.com>
2021-02-27 22:43:05 +0600

    Added animation. Added open & close functions. Made them uninteraptable

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,43 +1,47 @@
 function _createModal(options) {
     const modal = document.createElement("div","a-modal")
     modal.classList.add("a-modal")
-    modal.insertAdjacentHTML("afterbegin", `
-        <div class="modal-overlay">
-            <div class="modal-window">
-            
-                <div class="modal-header">
-                    <span class="modal-title">Modal title</span>
-                    <span class="modal-close">$tldr;</span>
+    modal.insertAdjacentHTML("beforeend", `
+            <div class="modal-overlay">
+                <div class="modal-window">
+
+                    <div class="modal-header">
+                        <span class="modal-title">Modal title</span>
+                        <span class="modal-close" onclick="modal.close()">&times;</span>
+                    </div>
+
+                    <div class="modal-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button class="btn btn-primary">Ok</button>
+                        <button class="btn">Cancel</button>
+                    </div>
+
                 </div>
-                
-                <div class="modal-body">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-                
-                <div class="modal-footer">
-                    <button class="btn btn-primary">Ok</button>
-                    <button class="btn">Cancel</button>
-                </div>
-                
-            </div>  
-        </div>
-    `)
+            </div>
+        `)
     console.log(modal)
     document.body.appendChild(modal)
     return modal
 }
 
 $.modal = function(options) {
-
+    const ANIMATION_SPEED = 300
     const $modal = _createModal(options)
 
     return {
         open() {
-
+            $modal.classList.add("open")
         },
         close() {
-
+            $modal.classList.remove("open")
+            $modal.classList.add("close")
+            setTimeout(() => {
+                $modal.classList.remove("close")
+            },ANIMATION_SPEED)
         },
         destroy() {
 
